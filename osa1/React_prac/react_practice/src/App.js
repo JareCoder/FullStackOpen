6783e4a7adf62b24fcc6cf02c97615d66ee9a1f3
JareCoder <jesse.m.lappalainen@gmail.com>
2023-05-25 00:28:59 +0300

    1d start

--- osa1/React_prac/react_practice/src/App.js
+++ osa1/React_prac/react_practice/src/App.js
@@ -1,28 +1,55 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
+const History = (props) => {
+  if (props.allClicks.length === 0){
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return(
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
 
-
-const Button = ({ handeClick, text }) => (
-    <button onClick={handeClick}>
-      {text}
-    </button>
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
 )
 
 function App(){
-  const [ counter, setCounter ] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
-  const increaseByOne = () => setCounter(counter +1)
-  const decreaseByOne = () => setCounter(counter - 1)
-  const setToZero = () => setCounter(0)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    setTotal(left + right)
+  }
 
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button handeClick={increaseByOne} text='plus' />
-      <Button handeClick={setToZero} text='zero' />
-      <Button handeClick={decreaseByOne} text='minus' />
+      <div>
+        {left}
+        <Button handleClick={handleLeftClick} text='left' />
+        <Button handleClick={handleRightClick} text='right' />
+        {right}
+        <History allClicks={allClicks} />
+      </div>
     </div>
   )
 }
