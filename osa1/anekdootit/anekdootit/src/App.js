05498f4b7648637b44bd36dfb61a1e5212356a55
JareCoder <jesse.m.lappalainen@gmail.com>
2023-05-26 23:39:49 +0300

    Finished anekdootit step3

--- osa1/anekdootit/anekdootit/src/App.js
+++ osa1/anekdootit/anekdootit/src/App.js
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 const increaseVote = ({ index, array }) => {
-  console.log(array)
   const copy = [...array]
   copy[index] += 1
 
@@ -10,6 +9,18 @@ const increaseVote = ({ index, array }) => {
 
 const CurrentVotes = ({ index, array }) => {
   return <p>This anecdote has {array[index]} votes.</p>
+}
+
+const findIndexOfMaxNum = ({ array }) => {
+  let indexOfMax = array.reduce((maxI, n, i, array) => {
+    if (n > array[maxI]){
+      return i
+    }else{
+      return maxI
+    }
+  }, 0)
+
+  return indexOfMax
 }
 
 function App() {
@@ -26,15 +37,18 @@ function App() {
 
   const [selected, setSelected] = useState(Math.floor(Math.random()*8))
   const [points, setPoints] = useState(new Array(8).fill(0))
-
-  console.log(points)
+  const topAnecdote = findIndexOfMaxNum({array: points})
 
   return (
     <div>
-      <h3>{anecdotes[selected]}</h3>
+      <h1>Anecdote of the day</h1>
+      <h4>{anecdotes[selected]}</h4>
       <CurrentVotes index={selected} array={points} />
       <button onClick={() => {setPoints(increaseVote({index: selected, array: points}))}}>Vote</button>
-      <button onClick={() => {setSelected(Math.floor(Math.random()*8))}}>Roll again</button>
+      <button onClick={() => {setSelected(Math.floor(Math.random()*8))}}>Roll for another anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <h4>{anecdotes[topAnecdote]}</h4>
+      <CurrentVotes index={topAnecdote} array={points} />
     </div>
   )
 }
