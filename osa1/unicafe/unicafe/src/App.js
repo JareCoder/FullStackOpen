b41a24dab64b6e4c343bdbb7fbb38bedd3e4c991
JareCoder <jesse.m.lappalainen@gmail.com>
2023-05-26 21:29:46 +0300

    Finished step3-5

--- osa1/unicafe/unicafe/src/App.js
+++ osa1/unicafe/unicafe/src/App.js
@@ -6,15 +6,40 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const StatisticLine = ({ text, val}) =>{
+  if(text === "Positive")
+    return(<p>{text}: {val}%</p>)
+  return(<p>{text}: {val}</p>)
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  let total = good + neutral + bad
+  let avg = (good - bad) / total
+  let positive = (good / total)*100
+
+  if(total === 0)
+    return(<p>No Data</p>)
+
+  return(
+    <div>
+      <h1>Statistics</h1>
+      <StatisticLine text="Good" val={good} />
+      <StatisticLine text="Neutral" val={neutral} />
+      <StatisticLine text="Bad" val={bad} />
+      <StatisticLine text="Total" val={total} />
+      <StatisticLine text="Average" val={avg} />
+      <StatisticLine text="Positive" val={positive} />
+    </div>
+  )
+}
+
 
 function App() {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  let total = good + neutral + bad
-  let avg = (good - bad) / total
-  let positive = (good / total)*100
+
 
 
   return (
@@ -23,13 +48,7 @@ function App() {
       <Button handleClick={() =>{setGood(good+1)}} text="Good" />
       <Button handleClick={() =>{setNeutral(neutral+1)}} text="Neutral" />
       <Button handleClick={() =>{setBad(bad+1)}} text="Bad" />
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>Total: {total}</p>
-      <p>Average: {avg}</p>
-      <p>Positive: {positive}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 }
